bc665288aaf8f761f07e06b7cf37266b118f86ea
Jackie <jacquelinewu88@gmail.com>
2024-07-07 16:30:11 -0400

    import rnclient content

---
+++ rnclient/ios/rnclient/rnclientApp.swift
@@ -0,0 +1,17 @@
+//
+//  rnclientApp.swift
+//  rnclient
+//
+//  Created by Jackie Wu on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct rnclientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
