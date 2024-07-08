c9228a2558df404e081445077e5f01020eb0d3fe
Jackie <jacquelinewu88@gmail.com>
2024-07-08 12:40:24 -0400

    add getting location

--- rnclient/jest.config.js
+++ rnclient/jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: 'react-native',
 };
+import mockRNDeviceInfo from "react-native-device-info/jest/react-native-device-info-mock"
+jest.mock("react-native-device-info", () => mockRNDeviceInfo)
--- rnclient/screens/DispatcherScreen.tsx
+++ rnclient/screens/DispatcherScreen.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
-// import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-// import {useNavigation} from '@react-navigation/native';
-
-// type RootStackParamList = {
-//   Home: undefined;
-//   Dispatcher: undefined;
-//   Responder: undefined;
-//   Caller: undefined;
-// };
-
-// type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Dispatcher'>;
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import * as Location from 'expo-location';
 
 const DispatcherScreen: React.FC = () => {
-        // const navigation = useNavigation<NavigationProp>();
+  const [location, setLocation] = useState<any>(null);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [latitude, setLatitude] = useState<any>(null);
+  const [longitude, setLongitude] = useState<any>(null);
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      try {
+        const {status} = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Location permission not granted');
+          throw new Error('Location permission not granted');
+        }
+        const location = await Location.getCurrentPositionAsync({});
+        console.log('Current location:', location);
+        setLatitude(location.coords.latitude);
+        setLongitude(location.coords.longitude);
+        setLocation(location);
+      } catch (error) {
+        console.error('Error requesting location permission:', error);
+      }
+    };
+    console.log('use effect request location');
+    requestLocationPermission();
+  }, []);
+
+  let text = 'Waiting for location...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
     <View style={styles.container}>
       <Text>Dispatcher Screen</Text>
+      <Text>{latitude}</Text>
+      <Text>{longitude}</Text>
     </View>
   );
 };
