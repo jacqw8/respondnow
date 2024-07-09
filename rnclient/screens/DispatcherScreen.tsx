2d7045c574a007169282d1db6b22f38d80ddcab7
Jackie <jacquelinewu88@gmail.com>
2024-07-09 10:19:30 -0400

    flesh out firebase auth sign up

--- rnclient/screens/DispatcherScreen.tsx
+++ rnclient/screens/DispatcherScreen.tsx
@@ -6,9 +6,11 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
-import * as Location from 'expo-location';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {auth} from '../firebase';
+import {onAuthStateChanged} from 'firebase/auth';
 
+// Screens
 import InfoScreen from './dispatcherscreens/InfoScreen';
 import DispatcherMapScreen from './dispatcherscreens/DispatcherMapScreen';
 import AuthScreen from './dispatcherscreens/AuthScreen';
@@ -16,9 +18,22 @@ import AuthScreen from './dispatcherscreens/AuthScreen';
 const Tab = createBottomTabNavigator();
 
 const DispatcherScreen: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setIsLoggedIn(!!user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (!isLoggedIn) {
+    return <AuthScreen />;
+  }
   return (
     <Tab.Navigator>
-        <Tab.Screen name="Login" component={AuthScreen} />
+      <Tab.Screen name="Login" component={AuthScreen} />
       <Tab.Screen name="Info" component={InfoScreen} />
       <Tab.Screen name="Map" component={DispatcherMapScreen} />
     </Tab.Navigator>
--- rnclient/screens/ResponderScreen.tsx
+++ rnclient/screens/ResponderScreen.tsx
@@ -6,17 +6,36 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
-import * as Location from 'expo-location';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {auth} from '../firebase';
+import {
+  onAuthStateChanged,
+} from 'firebase/auth';
 
+// Screens
 import ResponderInfoScreen from './responderscreens/ResponderInfoScreen';
 import ResponderMapScreen from './responderscreens/ResponderMapScreen';
+import AuthScreen from './responderscreens/AuthScreen';
 
 const Tab = createBottomTabNavigator();
 
 const ResponderScreen: React.FC = () => {
+        const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setIsLoggedIn(!!user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (!isLoggedIn) {
+    return <AuthScreen />;
+  }
   return (
     <Tab.Navigator>
+        <Tab.Screen name="Loging" component={AuthScreen} />
       {/* <Tab.Screen name="Info" component={ResponderInfoScreen} /> */}
       <Tab.Screen name="Map" component={ResponderMapScreen} />
     </Tab.Navigator>
